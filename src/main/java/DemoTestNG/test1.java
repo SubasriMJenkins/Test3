b47914b63a9f1e678fe33fcda8022d75dbbdcd6c
SubasriMJenkins <subasri@velammal.edu.in>
2024-09-03 15:47:03 +0530

    test1

---
+++ src/main/java/DemoTestNG/test1.java
@@ -0,0 +1,25 @@
+package DemoTestNG;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class test1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+		WebDriver driver = null;
+		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Subasri\\Downloads\\geckodriver\\geckodriver.exe");
+		driver= new FirefoxDriver();
+		driver.navigate().to("https://uat.karkakasadara.in/");
+		System.out.println(driver.getTitle());
+		wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+
+	}
+
+}
---
+++ src/test/java/com/tests/test1.java
@@ -0,0 +1,13 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+	@Test
+	public void test123() {
+		
+		System.out.println("Enter into VDA UAT login page");
+	}
+	
+}
