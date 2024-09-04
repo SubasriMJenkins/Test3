8ef77b2a78baf0efe27d55d1d7954afb9b193fe1
SubasriMJenkins <subasri@velammal.edu.in>
2024-09-04 14:32:44 +0530

    second commit

--- src/main/java/DemoTestNG/test1.java
+++ src/main/java/DemoTestNG/test1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class test1 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub		
 		
 		WebDriver driver = null;
@@ -17,6 +17,7 @@ public class test1 {
 		driver= new FirefoxDriver();
 		driver.navigate().to("https://uat.karkakasadara.in/");
 		System.out.println(driver.getTitle());
+		Thread.sleep(1000);
 		wait= new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 
